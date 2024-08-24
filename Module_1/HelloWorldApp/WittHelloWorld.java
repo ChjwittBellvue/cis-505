dae61e2e6ca9d49c3deeeabcd8df3a8ab6dfb6cf
Christopher <chjwitt@my365.bellevue.edu>
2024-08-24 16:10:36 -0500

    Modified solution in Hello World file to use parameters instead of pulling from class constants

--- Module_1/HelloWorldApp/WittHelloWorld.java
+++ Module_1/HelloWorldApp/WittHelloWorld.java
@@ -1,26 +1,23 @@
 /*
  *  REFERENCES
  * 
- *  Payne, D. (2021). CSD 405 Intermediate Java Programming. Bellevue University. Modified by Witt, C. 2024
+ *  Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
  *  Witt, C. (2024). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
  */
 
 package Module_1.HelloWorldApp;
 
 public class WittHelloWorld {
-  // Constants
-  final static String FIRST_NAME = "Christopher";
-  final static String LAST_NAME = "Witt";
 
   // Runs the program.
   public static void main(String[] args) {
-    System.out.printf(helloWorldStringBuilder());
+    System.out.printf(helloWorldStringBuilder("Christopher", "Witt"));
   } // end main
 
   /*
    * Builds hello world String from class constants.
    */
-  private static String helloWorldStringBuilder() {
-    return "Hello World from " + FIRST_NAME + " " + LAST_NAME + ".";
+  private static String helloWorldStringBuilder(String firstName, String lastName) {
+    return "Hello World from " + firstName + " " + lastName + ".";
   } // end helloWorldStringBuilder
 }
