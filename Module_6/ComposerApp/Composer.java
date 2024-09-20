87ffdf1a551813d466f35679af72dba8778633e1
Christopher <chjwitt@my365.bellevue.edu>
2024-09-20 10:22:53 -0500

    Added Module_6 package and Composer object

---
+++ Module_6/ComposerApp/Composer.java
@@ -0,0 +1,50 @@
+/*
+ *  REFERENCES
+ * 
+ *  Witt, C. (2024). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+ */
+package Module_6.ComposerApp;
+
+public class Composer {
+    // Attributes
+    private Integer id;
+    private String name;
+    private String genre;
+
+    // Constructors
+    public Composer() {
+    } // end default constructor
+
+    public Composer(Integer id, String name, String genre) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+    } // end full constructor
+
+    // Accessors
+    public Integer getId() {
+        return id;
+    } // end getId
+
+    public String getName() {
+        return name;
+    } // end getName
+
+    public String getGenre() {
+        return genre;
+    } // end getGenre
+
+    /**
+     * Overridden toString method displays transaction information with each
+     * attribute on its own line
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("  ID: " + id + System.lineSeparator());
+        sb.append("  Name: " + name + System.lineSeparator());
+        sb.append("  Genre: " + genre + System.lineSeparator());
+        return sb.toString();
+    } // end toString
+
+}
